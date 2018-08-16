cb1cd6dd0e7fbfa663b6d387875c12d84e5ac5a7
KristijanIvancic <kristijan.ivancic90@gmail.com>
2018-08-16 21:27:10 +0100

    Part 10: Add requesting password reset

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -17,6 +18,7 @@ login.login_view = 'login'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 from app import routes, models, errors
 
